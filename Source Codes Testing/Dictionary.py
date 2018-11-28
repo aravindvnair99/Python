098ed5d391f0e62d4950ca80cc57a032c65d1637
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-11-28 21:59:51 +0530

    Update and rename dictonry1.py to Dictionary.py

---
+++ Source Codes Testing/Dictionary.py
@@ -0,0 +1,8 @@
+dictionary =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+dictionary["year"] = 2018
+print("The dictionary is: ", dictionary)
+print("The list is: ", list(dictionary))
--- Source Codes Testing/dictonry1.py
+++
@@ -1,6 +0,0 @@
-d=dict()
-d['R']=1
-d['B']=2
-d['C']=3
-print(d)
-print(list(d))
