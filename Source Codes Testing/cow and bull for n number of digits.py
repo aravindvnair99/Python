73b26fc24ab66eb8cd1485bc197bcd1aedd62ac3
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2021-03-17 18:13:29 +0530

    Refactor
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- Source Codes Testing/cow and bull for n number of digits.py
+++ Source Codes Testing/cow and bull for n number of digits.py
@@ -1,37 +1,30 @@
 import random as ro
 
 
-def cow_bull(na,n):
-    un=input("enter a number you guess")
-    ua=list(str(un))
-    cow=0
-    bull=0
+def cow_bull(na, n):
+    un = input("enter a number you guess")
+    ua = list(str(un))
+    cow = 0
+    bull = 0
     for i in ua:
         if int(i) in na:
-            cow+=1
+            cow += 1
 
-
-
-            
     for i in range(n):
-        if na[i]==int(ua[i]):
-            bull+=1
-            cow-=1
+        if na[i] == int(ua[i]):
+            bull += 1
+            cow -= 1
 
-
-            
-
-    print("cow  :{0},bull  :{1}".format(cow,bull)) 
+    print("cow  :{0},bull  :{1}".format(cow, bull))
     return bull
 
 
-
-n=int(input("how many digits number you need? "))
-na=ro.sample(range(10),n)
-a=map(str,na)
-answer=''.join(a)
-ans=cow_bull(na,n)
-while ans!=n:
-    ans=cow_bull(na,n)
-print("answer",answer)
+n = int(input("how many digits number you need? "))
+na = ro.sample(range(10), n)
+a = map(str, na)
+answer = ''.join(a)
+ans = cow_bull(na, n)
+while ans != n:
+    ans = cow_bull(na, n)
+print("answer", answer)
 print("YOU WON!")
